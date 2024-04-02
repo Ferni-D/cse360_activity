007664a053f6e4185491814fd83653eaed517817
joeldiaz <joeldiaz@joels-mbp.dhcp.asu.edu>
2024-04-02 14:20:05 -0700

    Message

---
+++ cse360_activity/src/cse360_activity/CSE360Activity.java
@@ -0,0 +1,10 @@
+package cse360_activity;
+
+public class CSE360Activity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
